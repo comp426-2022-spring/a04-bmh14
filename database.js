cd1311bf6a986b9d285ac7c45b5d61d1f07e87f2
Bodhi Harmony <69818328+bmh14@users.noreply.github.com>
2022-04-29 14:17:27 -0400

    checking

--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// Require better-sqlite3
 const Database = require('better-sqlite3');
 
 const db = new Database('log.db');
@@ -9,12 +8,25 @@ const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and n
 
 let row = stmt.get();
 
-if (row === undefined) {
-    console.log('Database empty');
+if(row === undefined) {
+    console.log('Log database missing');
+
     const sqlInit = `
-        CREATE TABLE accesslog (id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time TEXT, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, status INTEGER, referer TEXT,  useragent TEXT);
-        `;
-        db.exec(sqlInit)
+    CREATE TABLE accesslog ( 
+        id INTEGER PRIMARY KEY, 
+        remoteaddr TEXT,
+        remoteuser TEXT,
+        time TEXT,
+        method TEXT,
+        url TEXT,
+        protocol TEXT,
+        httpversion TEXT,
+        status TEXT, 
+        referrer TEXT,
+        useragent TEXT
+    );
+    `;
+    db.exec(sqlInit)
 } else {
     console.log('Database exists.')
 }
